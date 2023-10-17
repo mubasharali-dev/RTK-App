5af27fec508dd4e07054400e64554ca93f8ff4b9
mubasharali-dev <mubashar.hashmat@gmail.com>
2023-10-17 11:39:10 +0500

    authentication added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import {Link, useLocation} from "react-router-dom";
 import "./Header.scss";
 import SearchBar from "../SearchBar/SearchBar.jsx";
+import {useAuth0} from "@auth0/auth0-react";
 
 const Header = () => {
+    const {loginWithRedirect, logout, isAuthenticated, user} = useAuth0();
     const location = useLocation();
     const currentPath = location.pathname;
 
-    // Function to determine if the search bar should be shown
     const shouldShowSearchBar = () => {
         return currentPath === '/';
     };
@@ -18,11 +19,30 @@ const Header = () => {
                 <div className="logo">Movie App</div>
             </Link>
             {shouldShowSearchBar() && <SearchBar/>}
-            <div className="user-image">
-                <img
-                    src="https://raw.githubusercontent.com/dmalvia/React_Redux_ToolKit_Movie_App/master/src/images/user.png"
-                    alt="user"
-                />
+            <div className="right-navbar">
+                <div className="user-profile">
+                    {
+                        isAuthenticated && (
+                            <div className="user-name">
+                                <span className="user-nickname">Welcome, {user.nickname || user.given_name}!</span>
+                            </div>
+                        )
+                    }
+                </div>
+                {
+                    isAuthenticated ? (
+                        <div>
+                            <button onClick={() => logout({logoutParams: {returnTo: window.location.origin}})}>
+                                Log Out
+                            </button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={() => loginWithRedirect()}>Log In</button>
+                        </div>
+                    )
+
+                }
             </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import {Provider} from "react-redux";
 import {store} from "./features/store.jsx";
+import {Auth0Provider} from '@auth0/auth0-react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <Auth0Provider
+        domain="dev-glgsazw7j5moht2e.us.auth0.com"
+        clientId="2QuA8SvrTbmrDxlT447suw224dWBnhu0"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
         <Provider store={store}>
             <App/>
         </Provider>
-    </React.StrictMode>,
+    </Auth0Provider>,
 )
